2a37e2835740f6689b9a58853d582017ea05dc82
Dantefdz <88099408+Dantefdz@users.noreply.github.com>
2022-04-13 22:57:53 -0300

    practica 3 terminada

--- Practica 3/ejer6/ejer6.pas
+++ Practica 3/ejer6/ejer6.pas
@@ -1,158 +1,148 @@
-program sex(seis);
-const 
-    valorAlto=9999;
-type 
-    prenda= record
-        cod:integer;
-        desc:string;
-        color:string;
-        tipo:string;
-        stock:integer;
-        precio: real;
-    end;
-    maestro= file of prenda;
-    detalle= file of integer;
+{6. Una cadena de tiendas de indumentaria posee un archivo maestro no ordenado
+con la información correspondiente a las prendas que se encuentran a la venta. De
+cada prenda se registra: cod_prenda, descripción, colores, tipo_prenda, stock y
+precio_unitario. Ante un eventual cambio de temporada, se deben actualizar las prendas
+a la venta. Para ello reciben un archivo conteniendo: cod_prenda de las prendas que
+quedarán obsoletas. Deberá implementar un procedimiento que reciba ambos archivos
+y realice la baja lógica de las prendas, para ello deberá modificar el stock de la prenda
+correspondiente a valor negativo.
+Por último, una vez finalizadas las bajas lógicas, deberá efectivizar las mismas
+compactando el archivo. Para ello se deberá utilizar una estructura auxiliar, renombrando
+el archivo original al finalizar el proceso.. Solo deben quedar en el archivo las prendas
+que no fueron borradas, una vez realizadas todas las bajas físicas}
 
-procedure leer_prenda(var p:prenda);
+program sex(seis);
+type
+    prenda = record
+        cod_prenda: integer;
+        descripcion: string[50];
+        colores: string;
+        tipo_prenda: string;
+        stock: integer;
+        precio_unitario: real;
+    end;
+
+    archivo_prendas = file of prenda;
+    archivo_detalle = file of integer;
+
+
+procedure leer_prenda(var a: prenda);
 begin
-    with p do begin
-        write('Ingresar codigo de prenda: ');
-        readln(cod);
-        if(cod <> -1)then begin
+    with a do begin
+        write('Ingresar codigo de la prenda: ');
+        readln(cod_prenda);
+        if(cod_prenda <> 0)then begin
             write('Ingresar descripcion de la prenda: ');
-            readln(desc);
-            write('ingresar el color de la prenda: ');
-            readln(color);
-            write('ingresar el tipo de la prenda: ');
-            readln(tipo);
-            write('ingresar el stock de la prenda: ');
+            readln(descripcion);
+            write('Ingresar colores de la prenda: ');
+            readln(colores);
+            write('Ingresar tipo de prenda: ');
+            readln(tipo_prenda);
+            write('Ingresar stock: ');
             readln(stock);
-            write('ingresar el precio de la prenda: ');
-            readln(precio);
+            write('Ingresar precio unitario: ');
+            readln(precio_unitario);          
         end;
     end;
 end;
 
-procedure crear_archivo(var a: maestro);
-var 
+procedure crear_maestro(var master:archivo_prendas);
+var
+    rec: prenda;
+begin
+    rewrite(master);
+    leer_prenda(rec);
+    while(rec.cod_prenda <> 0)do begin
+        write(master, rec);
+        leer_prenda(rec);
+    end;
+    close(master);
+end;
+
+procedure actualizar_maestro(var m: archivo_prendas; var d: archivo_detalle);
+var
+    cod: integer;
+    reg_m: prenda;
+begin
+    reset(m);
+    reset(d);
+    while(not eof(d))do begin   
+        read(d, cod);
+        while(not eof(m))do begin
+            read(m, reg_m);
+            if(reg_m.cod_prenda = cod)then begin
+                reg_m.stock := -1;
+                seek(m, filepos(m)-1);
+                write(m, reg_m);
+            end;
+        end;
+        seek(m, 0); 
+    end;
+    close(d);
+    close(m);
+end;
+
+procedure crear_detalle(var d:archivo_detalle);
+var
+    cod:integer;
+begin
+    rewrite(d);
+    writeln('Ingresar el codigo de las prendas que seran obsoletas: (finaliza con 0)');
+    write('Ingresar Codigo de prenda: ');
+    readln(cod);
+    while(cod <> 0)do begin
+        write(d, cod);
+        write('Ingresar Codigo de prenda: ');
+        readln(cod);
+    end;
+    close(d);
+end;
+
+procedure compactacion(var new_file: archivo_prendas; var m: archivo_prendas);
+var
     regm: prenda;
 begin
-    rewrite(a);
-    regm.cod := 0;
-    write(a, regm);
-    leer_prenda(regm);
-    while(regm.cod <> -1) do begin
-        write(a, regm);
-        leer_prenda(regm);
+    rewrite(new_file);
+    reset(m);
+    while(not eof(m))do begin
+        read(m, regm);
+        if(regm.stock >= 0)then 
+            write(new_file, regm);
     end;
-    close(a);
+    close(new_file);
+    close(m);
 end;
 
-procedure importarMaestro(var mae:maestro);
-var 
-    carga:text;
-    p:prenda;
-begin
-    assign(carga,'ejer6.txt');
-    reset(carga); rewrite(mae);
-    while not eof(carga) do begin
-        with p do begin
-            readln(carga,cod,stock,desc);
-            readln(carga,precio,color);
-            readln(carga,tipo);
-        end; 
-        write(mae,p);
-    end;
-    close(mae); close(carga);
-end;
-
-procedure importarDetalle(var det:detalle);
-var 
-    carga:text;
-    p:integer;
-begin
-    assign(carga,'ejer6.txt');
-    reset(carga); rewrite(det);
-    while not eof(carga) do begin
-        readln(carga,p);
-        write(det,p);
-    end;
-    close(det); close(carga);
-end;
-{-------------------------}
-procedure leer(var a:detalle; var dato:integer);
-begin
-	if(not eof(a))then read(a,dato)
-		else dato:=valoralto;
-end;
-
-procedure leerM(var a:maestro; var dato:prenda);
-begin
-	if(not eof(a))then read(a,dato)
-		else dato.cod:=valoralto;
-end;
-
-
-procedure baja(var mae: maestro; var det: detalle);
+procedure imprimir(var archivo: archivo_prendas);
 var
-	regm: prenda;
-	regd: integer;
+    regm: prenda;
 begin
-	reset(mae); reset(det);
-	read(mae,regm);
-	leer(det,regd);
-	while(regd <> valoralto) do begin
-		while(regm.cod <> regd) do read(mae,regm);
-		writeln(regd,'- ',regm.cod);
-		regm.stock:=0;
-		seek(mae, filepos(mae)-1);
-		write(mae,regm);
-		leer(det,regd);
-	end;
-	close(det);
-	close(mae);
+    reset(archivo);
+    while(not eof(archivo))do begin
+        read(archivo, regm);
+        writeln('codigo ', regm.cod_prenda, ' stock ', regm.stock);
+    end;
+    close(archivo);
 end;
 
-procedure compactar(var mae,a:maestro);
-var 
-    p:prenda;
+var
+    maestro: archivo_prendas;
+    detalle: archivo_detalle;
+    nuevo: archivo_prendas;
 begin
-    rewrite(a); reset(mae);
-    leerM(mae,p);
-    while(p.cod <> valoralto)do begin
-        if(p.stock <> 0) then write(a,p);
-        leerM(mae,p);
-    end;
+    assign(maestro, 'maestro');
+    assign(detalle, 'detalle');
+    //  CREACION DE ARCHIVOS
     
-    close(mae);close(a);
-    Erase(mae);
-    Rename(a, 'punto6M.txt');
-end;
+    //crear_maestro(maestro);
+    //crear_detalle(detalle);
+    actualizar_maestro(maestro, detalle); // 
+    assign(nuevo, 'maestro_actualizado');
+    compactacion(nuevo, maestro);
 
-procedure listarCompactado(var a:maestro);
-var p:prenda;
-begin
-    reset(a);
-    leerM(a,p);
-    while p.cod <> valoralto do begin
-        writeln('CODIGO: ',p.cod,'- Descripcion: ',p.desc);
-        leerM(a,p);
-    end;    
-    close(a);
-end;
-
-
-var
-    mae,a:maestro;
-    det:detalle;
-begin
-    assign(mae, 'ejer6.txt');
-    crear_archivo(mae);
-    assign(det, 'ejer6.txt');
-    assign(a, 'ejer6.txt');
-    importarMaestro(mae);
-    importarDetalle(det);
-    baja(mae,det);
-    compactar(mae,a);
-    listarCompactado(a);
-end.
+    // PRUEBAS
+    
+    // writeln('ARCHIVO MAESTRO: ');
+    // imprimir(maestro);
+    // writeln('ARCHIVO COMPACTADO: ');
+    // imprimir(nuevo);
